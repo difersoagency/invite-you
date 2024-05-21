bf9519373c5822bc46bd0e76545c481d15f4d0ee
wisnusunadi <barto7773@gmail.com>
2024-05-21 13:41:07 +0700

    login

--- src/app/component/FieldText.tsx
+++ src/app/component/FieldText.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function FieldText(props:any) {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    if(props.onChange){
+      props.onChange(e.target.value);
+    }
+  }
+
+
+
   return (
     <div className='mt-4'>
       <label htmlFor={props.usefor} className='font-bold text-left text-xs'>{props.label}</label>
       <br />
-      <input type="text" name={props.usefor} id={props.usefor}  className='border-2 border-gold px-3 py-2 text-xs rounded-lg '/>
+      <input type="text" name={props.usefor} id={props.usefor}  className='border-2 border-gold px-3 py-2 text-xs rounded-lg ' placeholder={props.usefor}  value={props.value || value} // controlled component value
+        onChange={handleChange} />
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+"use client"
 import Image from "next/image";
 import FieldText from "./component/FieldText";
 import { Button } from "@nextui-org/react";
+import { useState } from "react";
+
+
 export default function Home(){
+
+  const [username,setUsername] = useState("");
+
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    console.log(username)
+  };
+
+
   return(
     <section className="flex items-center justify-center h-full">
       <div className="m-auto">
@@ -14,10 +29,10 @@ export default function Home(){
         />
         <h1 className="font-bold text-2xl mt-2 text-center">Login</h1>
 
-        <form action="" className="mt-5 text-center">
-          <FieldText usefor='username' label='User' />
-          <FieldText usefor='password' label='Password'/>
-          <button className="px-8 py-2 text-white font-bold mx-auto bg-gold mt-6 text-xs ">Login</button>
+        <form onSubmit={loginHandler} className="mt-5 text-center">
+          <FieldText usefor='username' label='Username'  value={username} onChange={setUsername}  />
+          <FieldText usefor='password' label='Password' value={username} onChange={setUsername} />
+          <button className="px-8 py-2 text-white font-bold mx-auto bg-gold mt-6 text-xs " type="submit">Login</button>
         </form>
       </div>
     </section>
