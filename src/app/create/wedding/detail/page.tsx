2949f78697cb7c5ce453a5ff8e21e4ae2dbdf2ca
Avond Studio <web@avondstudio.com>
2024-06-14 01:52:26 +0700

    fix form buat

--- src/app/create/wedding/detail/page.tsx
+++ src/app/create/wedding/detail/page.tsx
@@ -145,10 +145,27 @@ export default function Page() {
         <div className='px-10 py-7'>
             {/* Step Navigator */}
             {/* {stepList()} */}
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-10 py-10'>
-                    <FieldDetail usefor='pasangan' label='Nama Pasangan' desc='Tuliskan Nama Pasangan' placeholder='Pria & Wanita' type='text' value={namaPasangan} onChange={setNamapasangan}/>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-10 py-10 items-stretch'>
+          
+                    <div className='border border-gray px-4 py-7' >
+                        <label htmlFor='pesan' className='font-bold text-left text-xs'>Gambar Pasangan</label>
+                        <p className='text-gray text-[0.6rem] mb-2 '>Upload dan Masukkan Gambar Pasangan sebagai Gambar Utama</p>
+                        <input type="file" className='utama-pict text-xs'  
+                        onChange={(event) => {
+                            setGambarutamaView(URL.createObjectURL(event.target.files[0]))
+                                    return setGambarutama(event.target.files[0])    
+                                }} />
+                        {gambarUtamaView ? 
+                        <div className='mt-10 border border-gold border-dotted px-6 py-4 rounded-lg '>
+                            <label htmlFor='pesan' className='font-bold text-left text-xs'>Image Preview</label>
 
-                    <div>
+                            <div className='w-fit mt-4'>
+                                <img src={gambarUtamaView} alt=""  width={200} height={200}/>
+                            </div>
+                        </div> : ''
+                        }
+                    </div>
+                    <div className='border border-gray px-4 py-7'>
                         <label htmlFor='pengantar' className='font-bold text-left text-xs'>Musik</label>
                         <p className='text-gray text-[0.6rem] mb-2 '>Pilih Musik yang ingin Anda gunakan</p>
                         <Select label='Musik Pilihan' value={musik} onChange={(event) => setMusik(event.target.value)} >
@@ -159,8 +176,8 @@ export default function Page() {
                             ))}
                         </Select>
                     </div>
-
-                    <div>
+                    
+                    {/* <div>
                         <label htmlFor='pesan' className='font-bold text-left text-xs'>Gambar Utama</label>
                         <p className='text-gray text-[0.6rem] mb-2 '>Upload dan Masukkan Gambar Utama undangan Anda</p>
                         <input type="file" className='utama-pict text-xs'  
@@ -177,9 +194,9 @@ export default function Page() {
                             </div>
                         </div> : ''
                         }
-                    </div>
+                    </div> */}
 
-                    <div>
+                    <div className='border border-gray px-4 py-7'>
                         <label htmlFor='pesan' className='font-bold text-left text-xs'>Gambar Cover</label>
                         <p className='text-gray text-[0.6rem] mb-2 '>Upload dan Masukkan Gambar Cover undangan Anda</p>
                         <input type="file" className='cover-pict text-xs' 
@@ -199,10 +216,17 @@ export default function Page() {
                         : ''}
                     </div>
 
+                    <div>
+                        <label htmlFor='pengantar' className='font-bold text-left text-xs'>Kata Pengantar</label>
+                        <p className='text-gray text-[0.6rem] mb-2 '>Tuliskan Kata Kata Pengantar Anda</p>
+                        <textarea name="pengantar" id="pengantar" value={kataPengantar} onChange={(event) => setKatapengantar(event.target.value)}  className='border border-gold px-3 py-2 text-xs  w-full md:w-2/3' rows={10}></textarea>
+                    </div>
+
                     <div className='col-span-2 border border-gold px-8 py-5'>
-                        <label htmlFor='pesan' className='font-bold text-left text-xs'>Upload Foto Gallery</label>
+                        <input type="checkbox" name="galeri" id="galeri"  className='peer/galeri'/>
+                        <label htmlFor='galeri' className='font-bold text-left text-xs ml-3'>Upload Foto Gallery</label>
                         <br /><br />
-                        <input type="file" name="" id="" className='text-xs' multiple onChange={(event) => {
+                        <input type="file" name="" id="" className='text-xs peer-checked/galeri:block hidden' multiple onChange={(event) => {
                              const files = Array.from(event.target.files);
                                  setGalleryView(files)
                                 return setGallery(files)
@@ -224,25 +248,17 @@ export default function Page() {
                     : ''}
                     </div>
 
-                    <div>
-                        <label htmlFor='pengantar' className='font-bold text-left text-xs'>Kata Pengantar</label>
-                        <p className='text-gray text-[0.6rem] mb-2 '>Tuliskan Kata Kata Pengantar Anda</p>
-                        <textarea name="pengantar" id="pengantar" value={kataPengantar} onChange={(event) => setKatapengantar(event.target.value)}  className='border-2 border-gold px-3 py-2 text-xs rounded-lg w-full md:w-2/3' rows={10}></textarea>
-                    </div>
-
-                    <div>
-                        <label htmlFor='pesan' className='font-bold text-left text-xs'>Pesan - Pesan (Optional)</label>
-                        <p className='text-gray text-[0.6rem] mb-2 '>Tuliskan pesan - pesan Anda untuk tamu undangan</p>
-                        <textarea name="pesan" id="pesan" value={pesan}   onChange={(event) => setPesan(event.target.value)}  className='border-2 border-gold px-3 py-2 text-xs rounded-lg w-full md:w-2/3' rows={10}></textarea>
-                    </div>
+                   
 
                     
 
+
+                    
                     <div className='py-5'>
                         <h2 className='text-dark font-bold mb-5'>Data Pria</h2>
 
                         <div className=' px-6 py-7 border border-gold flex flex-col gap-7 w-full md:w-4/5'>
-                            <FieldDetail usefor='pria' label='Nama Pria' desc='' placeholder='Nama Pria' type='text' value={namaPria} onChange={setNamapria}/>
+                            <FieldDetail usefor='pria' label='Nama Panggilan Pria' desc='' placeholder='Nama Panggilan' type='text' value={namaPria} onChange={setNamapria}/>
 
                             <FieldDetail usefor='pria-lengkap' label='Nama Lengkap Pria' desc='' placeholder='Nama Lengkap Pria' type='text' value={namaLengkapPria} onChange={setNamalengkappria}/>
 
@@ -251,9 +267,10 @@ export default function Page() {
                             <FieldDetail usefor='ibu-pria' label='Nama Ibu (Pria)' desc='' placeholder='Nama Ibu Pria' type='text' value={ibuPria} onChange={setIbupria} />
 
                             <div>
-                                <label htmlFor='pesan' className='font-bold text-left text-xs'>Foto Pria</label>
+                                <input type="checkbox" name="fotop" id="fotop" className='peer/fotop'/>
+                                <label htmlFor='fotop' className='font-bold text-left text-xs ml-3'>Foto Pria</label>
                                 <p className='text-gray text-[0.6rem] mb-2 '>Masukkan Foto Pria</p>
-                                <input type="file" className='p-pict text-xs' 
+                                <input type="file" className='p-pict text-xs peer-checked/fotop:block hidden' 
                                 onChange={(event) => {
                                     setFotopriaView(URL.createObjectURL(event.target.files[0]))
                                     return setFotopria(event.target.files[0])
@@ -281,18 +298,18 @@ export default function Page() {
                         <h2 className='text-dark font-bold  mb-5'>Data Wanita</h2>
 
                         <div className=' px-6 py-7 border border-gold flex flex-col gap-7 w-full md:w-4/5'>
-                            <FieldDetail usefor='wanita'  value={namaWanita} onChange={setNamawanita} label='Nama wanita' desc='' placeholder='Nama wanita' type='text' />
+                            <FieldDetail usefor='wanita'  value={namaWanita} onChange={setNamawanita} label='Nama Panggilan Wanita' desc='' placeholder='Nama wanita' type='text' />
 
                             <FieldDetail usefor='wanita-lengkap' value={namaLengkapWanita} onChange={setNamalengkapwanita} label='Nama Lengkap wanita' desc='' placeholder='Nama Lengkap Wanita' type='text' />
 
                             <FieldDetail usefor='ayah-wanita'  value={ayahWanita} onChange={setAyahwanita} label='Nama Ayah (Wanita)' desc='' placeholder='Nama Ayah Wanita' type='text' />
 
                             <FieldDetail usefor='ibu-wanita' value={ibuWanita} onChange={setIbuwanita} label='Nama Ibu (Wanita)' desc='' placeholder='Nama Ibu Wanita' type='text' />
-
                             <div>
-                                <label htmlFor='pesan' className='font-bold text-left text-xs'>Foto Wanita</label>
+                                <input type="checkbox" name="fotow" id="fotow" className='peer/fotow'/>
+                                <label htmlFor='fotow' className=' font-bold text-left text-xs ml-3'>Foto Wanita</label>
                                 <p className='text-gray text-[0.6rem] mb-2 '>Masukkan Foto Wanita</p>
-                                <input type="file" className='w-pict text-xs' 
+                                <input type="file" className='w-pict text-xs hidden peer-checked/fotow:block' 
                                   onChange={(event) => {
                                     setFotowanitaView(URL.createObjectURL(event.target.files[0]))
                                     return setFotowanita(event.target.files[0])
@@ -312,10 +329,27 @@ export default function Page() {
                         </div>
                     </div>
 
+
                     <div className='py-5'>
                         <h2 className='text-dark font-bold  mb-5'>Data Akad / Pemberkatan</h2>
 
                         <div className=' px-6 py-7 border border-gold flex flex-col gap-7 w-full md:w-4/5'>
+                            <FieldDetail usefor='alamat' label='Alamat Akad' desc='' placeholder='Alamat Akad' type='text' value={alamatResepsi} onChange={setAlamatresepsi} />
+
+                            <FieldDetail usefor='tanggal' label='Tanggal Akad' desc='' placeholder='Tanggal / Bulan / Tahun' type='date' value={tglResepsi} onChange={setTglresepsi} />
+
+                            <FieldDetail usefor='waktu' label='Waktu Akad' desc='' placeholder='Jam Akad' type='time' value={waktuResepsi} onChange={setWakturesepsi} />
+
+                        </div>
+                    </div>
+
+                    <div className='py-5'>
+                    <h2 className='text-dark font-bold  mb-5'>Data Resepsi (Optional)</h2>
+                    <input type="checkbox" name="resepsi" id="resepsi" className='peer/active'/>
+                    <label htmlFor="resepsi" className='text-xs ml-2 peer-checked/active:text-gold '>Dengan Resepsi</label>
+                        
+
+                        <div className=' px-6 py-7 border border-gold flex-col gap-7 w-full md:w-4/5 hidden peer-checked/active:flex mt-5'>
                             <FieldDetail usefor='alamat' label='Alamat Resepsi' desc='' placeholder='Alamat Resepsi' type='text' value={alamatResepsi} onChange={setAlamatresepsi} />
 
                             <FieldDetail usefor='tanggal' label='Tanggal Resepsi' desc='' placeholder='Tanggal / Bulan / Tahun' type='date' value={tglResepsi} onChange={setTglresepsi} />
@@ -323,6 +357,9 @@ export default function Page() {
                             <FieldDetail usefor='waktu' label='Waktu Resepsi' desc='' placeholder='Waktu Resepsi' type='time' value={waktuResepsi} onChange={setWakturesepsi} />
 
                         </div>
+                    </div>
+                    <div >
+                        <FieldDetail usefor='pasangan' label='Nama Pasangan' desc='Tuliskan Nama Pasangan' placeholder='Nama Cowok Nama Cewek (David Yusi)' type='text' value={namaPasangan} onChange={setNamapasangan}/>
                     </div>
                 </div>
 
